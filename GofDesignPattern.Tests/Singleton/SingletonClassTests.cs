89ad9f4dd1510aad8984d252e09b27ab1156f59d
tetsu.k <bodomu@gmail.com>
2024-10-21 08:43:36 +0900

    [add] Singletonパターン

---
+++ GofDesignPattern.Tests/Singleton/SingletonClassTests.cs
@@ -0,0 +1,24 @@
+﻿using GofDesignPattern.Singleton;
+
+namespace GofDesignPattern.Tests.Singleton
+{
+    public class SingletonClassTests
+    {
+        /// <summary>
+        /// 同じインスタンスが取得できることを確認
+        /// </summary>
+        [Fact]
+        public void OkSingleton()
+        {
+            // Act
+            var singleton1 = SingletonClass.GetInstance();
+            var singleton2 = SingletonClass.GetInstance();
+
+            // new できないことを確認
+            //var single = new SingletonClass();
+
+            // Assert
+            Assert.Same(singleton1, singleton2);
+        }
+    }
+}
---
+++ GofDesignPattern/Singleton/SingletonClass.cs
@@ -0,0 +1,17 @@
+﻿namespace GofDesignPattern.Singleton
+{
+    public class SingletonClass
+    {
+        private readonly static SingletonClass singleton = new SingletonClass();
+
+        private SingletonClass()
+        {
+            // new でインスタンスを生成できないようにするために private でコンストラクタを定義
+        }
+
+        public static SingletonClass GetInstance()
+        {
+            return singleton;
+        }
+    }
+}
